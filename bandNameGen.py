278739a419d6f029cfa075c95614fac2b5719d0c
jadexavier <bdrager2@gmail.com>
2022-08-03 21:17:15 -0500

    vscode setup

---
+++ bandNameGen.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+user_town = input("What town did you grow up in?\n")
+user_animal = input("What is your favorite animal?\n")
+print("Your band name is " + user_town + " " + user_animal)
+
+    
+
